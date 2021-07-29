223521a667c7afccc8ebd54bf093ce1cebc3563b
arronpj <princejar@gmail.com>
2021-07-29 14:19:49 +0800

    support check platform in Kit

--- Sources/DeviceUtilKit/DeviceUtilKit.swift
+++ Sources/DeviceUtilKit/DeviceUtilKit.swift
@@ -6,6 +6,15 @@ public class DeviceUtilKit {
     //static let v_major
     public var version = "0.3.3"
     
+    public static var platform : String {
+        var result = ""
+        #if targetEnvironment(macCatalyst)
+            result = "macOS"
+        #else
+            result = "iOS/iPadOS"
+        #endif
+        return result
+    }
 }
 public struct DeviceUtil {
     
