69abdc7c5ffe82484c356fbb272b3e0837bf4595
arronpj <princejar@gmail.com>
2021-07-29 12:51:31 +0800

    Update DeviceUtilKit.swift

--- Sources/DeviceUtilKit/DeviceUtilKit.swift
+++ Sources/DeviceUtilKit/DeviceUtilKit.swift
@@ -4,14 +4,11 @@ import Foundation
 //
 public class DeviceUtilKit {
     //static let v_major
-    public var version = "0.2.1"
+    public var version = "0.3.3"
     
 }
 public struct DeviceUtil {
     
 }
 
-//version
-//0.1.0 : first API
-//0.1.1 : provide Hex String to UIColor
-//0.2.0 : provide pre-define color.
+
